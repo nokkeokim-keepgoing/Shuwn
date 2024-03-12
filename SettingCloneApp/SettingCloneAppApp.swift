27afc58cffb9c8da25f0264ad0a0ac5bc9983bb9
frankwon11 <frankwon11@gmaiil.com>
2024-03-13 01:16:12 +0900

    Initial Commit

---
+++ SettingCloneApp/SettingCloneAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SettingCloneAppApp.swift
+//  SettingCloneApp
+//
+//  Created by sseungwonnn on 3/13/24.
+//
+
+import SwiftUI
+
+@main
+struct SettingCloneAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
